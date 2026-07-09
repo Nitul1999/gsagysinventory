2d657dcf8e2a88623e6149c471c2e1481a4b6b56
nitul sonowal <nitulsonowal8133@gmail.com>
2026-07-09 21:22:54 +0530

    deployment

--- frontend/src/pages/adminpages/ViewNotVerifyitem.jsx
+++ frontend/src/pages/adminpages/ViewNotVerifyitem.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Table, message } from "antd";
+import { message } from "antd";
 import axiosInstance from "../../apicalls/axiosInstance";
 import { Viewnotreturncomp } from "../../component/admin/Viewnotreturncomp";
 
